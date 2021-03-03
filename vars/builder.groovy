ff1db7809ee52409c34952c50142a311ab4f7228
ori rothschild <ori.rothschild@automat-it.com>
2021-03-03 15:28:28 +0200

    mkdir if not exist

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -1,8 +1,7 @@
 def call(gradle_action,distname) {
    echo "Running ${gradle_action} with"
-   sh "mkdir dist"
+   sh "mkdir -p dist"
    sh "./gradlew ${gradle_action} --no-daemon"
     archiveArtifacts artifacts: "dist/${distname}"
     return "success"
 }
-\
