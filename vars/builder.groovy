a75b305f102394f733a8cb31e004ccad1dd2e1d3
ori rothschild <ori.rothschild@automat-it.com>
2021-03-03 15:20:22 +0200

    MKDIR DIST

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -1,6 +1,6 @@
 def call(gradle_action,distname) {
    echo "Running ${gradle_action} with"
-   sh mkdir dist
+   sh "mkdir dist"
    sh "./gradlew ${gradle_action} --no-daemon"
     archiveArtifacts artifacts: "dist/${distname}"
     return "success"
