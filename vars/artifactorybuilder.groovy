ce3bb76f2f8edc3f2b4fc158b50a979e6337ef2e
ori rothschild <ori.rothschild@automat-it.com>
2021-03-04 14:29:37 +0200

    changed artifactory builder

--- vars/artifactorybuilder.groovy
+++ vars/artifactorybuilder.groovy
@@ -1,6 +1,20 @@
-def call(gradle_action,distname) {
-   echo "Running ${gradle_action} with"
-   sh "mkdir -p dist"
-   sh "./gradlew ${gradle_action} --no-daemon"
-    archiveArtifacts artifacts: "dist/${distname}", followSymlinks: false
+def call(gradle_action,distname,path) {
+
+ if("${path}" == 'null') {
+        error 'Variable path is wrongly is not defined'
+    }
+    echo "Running ${gradle_action} with"
+    sh "mkdir -p dist"
+    sh "./gradlew ${gradle_action} --no-daemon"
+    path = remove_backslash(path);
+        archiveArtifacts artifacts
+        : "${path}/${distname}", followSymlinks: false
+    }
+}
+
+def remove_backslash(path) {
+    while("${path}".endWith('/')) {
+        path = path.substring(0, path.length() - 1)
+    }
+    return path
 }
