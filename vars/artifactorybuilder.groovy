7f7de754041b5c2a8cdae17ced3ceed949d29ce7
ori rothschild <ori.rothschild@automat-it.com>
2021-03-04 14:04:18 +0200

    added artiffactorybuild

--- vars/artifactorybuilder.groovy
+++ vars/artifactorybuilder.groovy
@@ -2,6 +2,5 @@ def call(gradle_action,distname) {
    echo "Running ${gradle_action} with"
    sh "mkdir -p dist"
    sh "./gradlew ${gradle_action} --no-daemon"
-    archiveArtifacts artifacts: "dist/${distname}"
-    return "success"
+    archiveArtifacts artifacts: "dist/${distname}", followSymlinks: false
 }
